463d91798e54e4099ec7ac48b89afb6ee9c8d0c4
alfn7511 <alfn7511@gmail.com>
2017-08-28 16:10:39 +0900

    update

---
+++ data/cache/latest-notice-basic-5-25.php
@@ -0,0 +1,83 @@
+<?php
+if (!defined('_GNUBOARD_')) exit;
+$bo_subject='공지사항';
+$list=array (
+  0 => 
+  array (
+    'wr_id' => '1',
+    'wr_num' => '-1',
+    'wr_reply' => '',
+    'wr_parent' => '1',
+    'wr_is_comment' => '0',
+    'wr_comment' => '0',
+    'wr_comment_reply' => '',
+    'ca_name' => '',
+    'wr_option' => '',
+    'wr_subject' => '브릿센트 사이트 개발 중 입니다.',
+    'wr_content' => '브릿센트 사이트 개발 중 입니다.',
+    'wr_link1' => '',
+    'wr_link2' => '',
+    'wr_link1_hit' => '0',
+    'wr_link2_hit' => '0',
+    'wr_hit' => '2',
+    'wr_good' => '0',
+    'wr_nogood' => '0',
+    'mb_id' => 'admin',
+    'wr_password' => '*9C27804D4481608689F457784403F060991485A9',
+    'wr_name' => '최고관리자',
+    'wr_email' => 'admin@domain.com',
+    'wr_homepage' => '',
+    'wr_datetime' => '2017-07-13 10:21:09',
+    'wr_file' => '0',
+    'wr_last' => '2017-07-13 10:21:09',
+    'wr_ip' => '1.223.164.173',
+    'wr_facebook_user' => '',
+    'wr_twitter_user' => '',
+    'wr_1' => '',
+    'wr_2' => '',
+    'wr_3' => '',
+    'wr_4' => '',
+    'wr_5' => '',
+    'wr_6' => '',
+    'wr_7' => '',
+    'wr_8' => '',
+    'wr_9' => '',
+    'wr_10' => '',
+    'is_notice' => true,
+    'subject' => '브릿센트 사이트 개발 중 입니다.',
+    'comment_cnt' => '',
+    'datetime' => '2017-07-13',
+    'datetime2' => '07-13',
+    'last' => '2017-07-13',
+    'last2' => '07-13',
+    'name' => '<span class="sv_member">최고관리자</span>',
+    'reply' => 0,
+    'icon_reply' => '',
+    'icon_link' => '',
+    'ca_name_href' => 'http://britcent.cafe24.com/bbs/board.php?bo_table=notice&amp;sca=',
+    'href' => 'http://britcent.cafe24.com/bbs/board.php?bo_table=notice&amp;wr_id=1',
+    'comment_href' => 'http://britcent.cafe24.com/bbs/board.php?bo_table=notice&amp;wr_id=1',
+    'icon_new' => '',
+    'icon_hot' => '',
+    'icon_secret' => '',
+    'link' => 
+    array (
+      1 => NULL,
+      2 => NULL,
+    ),
+    'link_href' => 
+    array (
+      1 => 'http://britcent.cafe24.com/bbs/link.php?bo_table=notice&amp;wr_id=1&amp;no=1',
+      2 => 'http://britcent.cafe24.com/bbs/link.php?bo_table=notice&amp;wr_id=1&amp;no=2',
+    ),
+    'link_hit' => 
+    array (
+      1 => 0,
+      2 => 0,
+    ),
+    'file' => 
+    array (
+      'count' => '0',
+    ),
+  ),
+)?>
